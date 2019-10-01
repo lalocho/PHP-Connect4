a98a58370522865070340182809cede1465bc4ae
Alex <avasquez31@miners.utep.edu>
2019-10-01 11:07:31 -0600

    Finished needs of info class. Need to learn how to check if it works.

--- src/info/index.php
+++ src/info/index.php
@@ -1,1 +1,23 @@
 <?php
+
+ class Info{
+
+    var $width; //board width
+    var $height; //board height
+    var $strategy; //strategy
+
+     //TODO: figure out logic for strategy
+
+    public function _construct($boardWidth, $boardHeight, $gameStrat){
+        $this -> width = $boardWidth;
+        $this -> height = $boardHeight;
+        $this -> strategy = $gameStrat;
+    }
+}
+
+$gameBoard = new Info();
+$gameBoard ->_construct(6,7,"Random");
+
+echo json_encode($gameBoard);
+
+?>
