f70653f46f4c5b91a51441cc5a244cca51519013
Alex <avasquez31@miners.utep.edu>
2019-10-01 11:42:33 -0600

    quick change to match example on assignment.

--- c4-home/info/index.php
+++ c4-home/info/index.php
@@ -4,20 +4,21 @@
 
     var $width; //board width
     var $height; //board height
-    var $strategy; //strategy
+    var $strategies; //strategy
 
      //TODO: figure out logic for strategy
 
     public function _construct($boardWidth, $boardHeight, $gameStrat){
         $this -> width = $boardWidth;
         $this -> height = $boardHeight;
-        $this -> strategy = $gameStrat;
+        $this -> strategies = $gameStrat;
     }
 }
 
 $gameBoard = new Info();
-$gameBoard ->_construct(6,7,"Random");
+$gameBoard ->_construct(6,7,["Smart", "Random"]);
 
 echo json_encode($gameBoard);
+
 
 ?>
