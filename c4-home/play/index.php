58097d9a17a05788bc39dde96959d860f8659404
Luis <lalocho13@gmail.com>
2019-10-02 19:44:23 -0600

    prototype

--- c4-home/play/index.php
+++ c4-home/play/index.php
@@ -4,8 +4,8 @@
 //class move will be the computer automated move
 
 //need methods isWin and isDraw (implement logic here)
-$PID =  $_Get["pid"];
-$MOVE = $_Get["move"];
+$PID =  $_GET["pid"];
+$MOVE = $_GET["move"];
 $url = dirname(dirname(__FILE__))."/writable/";
 $return = new ackmove();
 
@@ -21,18 +21,65 @@ if($return->reason){// if there is a reason output it and exit
 }
 $file = file_get_contents(url.$PID.".txt");
 $decoded_file = json_decode($file);
-global $game_board;
+$strategy = &$decoded_file->strategy;
 $game_board = &$decoded_file->gameboard;
+$response = &$decoded_file->response;
+check();
+if($strategy == "Random"){
+    $aimove = random_move();
+}else {
+    $aimove = smart_move();
+}
+$ai_return = new move();
 
+update_board($aimove);
+echo json_encode(array("response"=> $response,"ack_move" => array("slot"=> $MOVE,
+    "isWin"=> $return->userWin, "isDraw"=> $return->userDraw,
+    "row"=> "[]"), "move"=> array("slot"=> $aimove, "isWin"=>$ai_return->isWin,
+    "isDraw"=> $ai_return->isDraw, "row"=> "[]")));
+
+function smart_move(){
+    $smart_move = rand(0,6);
+    return $smart_move;
+}
+function random_move(){
+    $rand_move = rand(0,6);
+    while(isColumnFull($rand_move)){
+        $rand_move = rand(0,6);
+    }
+    return $rand_move;
+}
+function check($return)
+{
+    global $MOVE;
+    update_board($MOVE);
+    $return->userWin = isWin();
+    $return->userDraw = isDraw();
+    $return->slot = $MOVE;
+}
+function isWin(){
+    global $game_board;
+    return false;
+}
+function isDraw(){
+    for($i = 0; $i <=6; $i++){
+        if(isColumnFull($i)){
+            return false;
+        }
+    }
+    return true;
+}
 function isColumnFull($column){
-    if(global $game_board[0][$column]){
+    global $game_board;
+    if($game_board[0][$column]){
         return true;
     }else {return false;}
 }
 function update_board($input){
+    global $game_board;
     for( $i = 5; $i >=0;$i--){
-        if (global $game_board[$i][$input]== 0){
-            global $game_board[$i][$input]= 1;
+        if ($game_board[$i][$input]== 0){
+            $game_board[$i][$input]= 1;
             exit();
         }
     }
@@ -42,28 +89,19 @@ class ackmove{ //human user
     var $userWin;
     var $row; //indices fo winning row. [x1,y1,x2..,xn]
     var $reason;
+    var $isWin;
+    var $userDraw;
 
 }
 //NOTE: no computer move if human has game ending move
 
 class move{ //computer logic
-
+    var $isDraw;
+    var $isWin;
+    var $slot;
+    var $row;
 
 }
 
-function isWin(){
-
-    return(false);
-}
-
-function isDraw(){
-    //only draw when board is full and nobody as won
-    return (false);
-}
-
-while(1){
-    //visit the url
-    //if(ackmove::)
-}
 
 ?>
