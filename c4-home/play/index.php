303a89de7f6f09d29efd82dfc57db2e01a3a77c6
Luis <lalocho13@gmail.com>
2019-10-04 20:32:13 -0600

    Missing smart, and return indices. Everything else works.

--- c4-home/play/index.php
+++ c4-home/play/index.php
@@ -6,8 +6,14 @@
 //need methods isWin and isDraw (implement logic here)
 $PID =  $_GET["pid"];
 $MOVE = $_GET["move"];
+global $color;
+global $win_indices;
+
+$color= 1;
+$win_indices = array();
 $url = dirname(dirname(__FILE__))."/writable/";
 $return = new ackmove(); // possibly reason for error?
+
 
 //REASONS FOR GAME TO EXIT
 if (!$PID){
@@ -31,7 +37,7 @@ if (!$PID){
     echo json_encode($return);
     exit();
 }
-$return -> slot = $MOVE;
+$return -> slot = (int)$MOVE;
 $file = file_get_contents($url.$PID.".txt");
 $decoded_file = json_decode($file);
 $strategy = &$decoded_file->strategy;
@@ -41,6 +47,7 @@ $response = true;
 //echo json_encode(array($game_board));
 //echo json_encode(array($response));
 check($return);
+
 if($strategy == "Random"){
     $aimove = random_move();
 }else {
@@ -52,7 +59,6 @@ check($ai_return);
 
 
 
-
 $saved_board = $url.$PID.".txt";
 $opened_board = fopen($saved_board,"w");//creates new file
 $board_string = json_encode(array("pid"=>$PID, 'strategy'=> $strategy, 'board'=> $game_board));
@@ -61,10 +67,10 @@ fclose($opened_board);
 
 
 
-echo json_encode(array("response"=> $response,"ack_move" => array("slot"=> $MOVE,
+echo json_encode(array("response"=> $response,"ack_move" => array("slot"=> $return->slot,
     "isWin"=> $return->userWin, "isDraw"=> $return->userDraw,
-    "row"=> array(), "move"=> array("slot"=> $aimove, "isWin"=>$ai_return->userWin,
-    "isDraw"=> $ai_return->userDraw, "row"=> array()))));
+    "row"=> array()), "move"=> array("slot"=> $aimove, "isWin"=>$ai_return->userWin,
+    "isDraw"=> $ai_return->userDraw, "row"=> array())));
 
 function smart_move(){
     $smart_move = rand(0,6);
@@ -84,9 +90,56 @@ function check($return)
     $return->userDraw = isDraw();
 
 }
+function horizontal_win(){
+    global $game_board;
+    global $win_indices;
+    for ($i = 0; $i <= count($game_board)-1; $i++) {
+        for ($j = 0; $j <= count($game_board[$i]) - 3; $j++) {
+            if ($game_board[$i][$j] != '0' && $game_board[$i][$j] == $game_board[$i][$j + 1]
+                && $game_board[$i][$j] == $game_board[$i][$j + 2] && $game_board[$i][$j] == $game_board[$i][$j + 3]){
+                $win_indices = array($i=> $j,$i => $j+1, $i => $j+2, $i => $j+3);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+function vertical_win(){
+    global $game_board;
+    global $win_indices;
+    for ($i = 0; $i <= count($game_board)-1; $i++) {
+        for ($j = 0; $j <= count($game_board[$i]) - 3; $j++) {
+            if ($game_board[$i][$j] != '0' && $game_board[$i][$j] == $game_board[$i+1][$j]
+                && $game_board[$i][$j] == $game_board[$i+2][$j] && $game_board[$i][$j] == $game_board[$i+3][$j]){
+                $win_indices = array($i=> $j,$i+1 => $j, $i+2 => $j, $i+3 => $j);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+function diagonal_win(){
+    global $game_board;
+    for ($i = 0; $i <= count($game_board)-1; $i++) {
+        for ($j = 0; $j <= count($game_board[$i]) - 3; $j++) {
+            if ($game_board[$i][$j] != '0' && $game_board[$i][$j] == $game_board[$i+1][$j+1]
+                && $game_board[$i][$j] == $game_board[$i+2][$j+2] && $game_board[$i][$j] == $game_board[$i+3][$j+3]) {
+                $win_indices = array($i=> $j,$i+1 => $j+1, $i+2 => $j+2, $i+3 => $j+3);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+
 function isWin(){
     global $game_board;
-    return false;
+    if(horizontal_win() or vertical_win() or diagonal_win())
+        return true;
+    else
+        return false;
 }
 function isDraw(){
     for($i = 0; $i <=6; $i++){
@@ -106,11 +159,16 @@ function isColumnFull($column){ //works
 }
 function update_board($input){
     global $game_board;
+    global $color;
     for($i = 5; $i >=0;$i--){
         if ($game_board[$i][$input]== 0){
-            $game_board[$i][$input]= 1;
+            $game_board[$i][$input]= $color;
             $i = -1;
         }
+    }
+    $color++;
+    if($color >2){
+        $color = 1;
     }
 }
 class ackmove{ //human user
