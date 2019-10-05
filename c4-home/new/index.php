54c95f7da91818eb724747c9899394035e97b458
Luis <lalocho13@gmail.com>
2019-10-04 18:54:13 -0600

    Fixing responsee

--- c4-home/new/index.php
+++ c4-home/new/index.php
@@ -12,26 +12,6 @@ $strategy = $_GET["strategy"];
 
 $currentGame = new Game(); //instance
 $strategies = array("Smart", "Random");
-
-
-if(in_array($strategy,$strategies)){ //if Smart or Random
-    $currentGame->response = true;
-    $currentGame->pid = uniqid();
-    echo json_encode( array("response"=> $currentGame->response, "pid"=> $currentGame->pid));
-    $currentGame->gameBoard = $emptyBoard; //set a new board
-
-}else{ //User strategy not available
-    $currentGame->response = false;
-    $currentGame->reason = "No such strategy";
-    echo json_encode(array("response" => $currentGame->response, "reason" => $currentGame->reason));
-}
-if($currentGame -> response == true){ //set a new game
-    $saved_board = $url.$currentGame->pid."txt";
-    $opened_board = fopen($saved_board,"w");//creates new file
-    $board_string = json_encode(array("pid"=>$currentGame->pid, 'strategy'=> Strategy, 'board'=> $currentGame->gameBoard));
-    fwrite($opened_board,$board_string);
-    fclose($opened_board);
-}
 $emptyBoard= array( //new game board
     array(0,0,0,0,0,0,0,),
     array(0,0,0,0,0,0,0,),
@@ -39,5 +19,26 @@ $emptyBoard= array( //new game board
     array(0,0,0,0,0,0,0,),
     array(0,0,0,0,0,0,0,),
     array(0,0,0,0,0,0,0,));
+
+if(in_array($strategy,$strategies)){ //if Smart or Random
+    $currentGame->response = true;
+    $currentGame->pid = uniqid();
+    echo json_encode( array("response"=> $currentGame->response, "pid"=> $currentGame->pid));
+
+
+}else{ //User strategy not available
+    $currentGame->response = false;
+    $currentGame->reason = "No such strategy";
+    echo json_encode(array("response" => $currentGame->response, "reason" => $currentGame->reason));
+}
+if($currentGame -> response == true){ //set a new game
+    $saved_board = $url.$currentGame->pid.".txt";
+    $opened_board = fopen($saved_board,"w");//creates new file
+    $currentGame->gameBoard = $emptyBoard; //set a new board
+    $board_string = json_encode(array("pid"=>$currentGame->pid, 'strategy'=> $strategy, 'board'=> $currentGame->gameBoard));
+    fwrite($opened_board,$board_string);
+    fclose($opened_board);
+}
+
 
 ?>
--- c4-home/play/index.php
+++ c4-home/play/index.php
@@ -12,41 +12,59 @@ $return = new ackmove(); // possibly reason for error?
 //REASONS FOR GAME TO EXIT
 if (!$PID){
     $return->reason = "The PID is not valid";
+    $return -> response = false;
     echo json_encode($return);
     exit();
 }else if ($MOVE > 6){   //must be in range 0-6
     $return -> reason = "Must select valid slot.";
+    $return -> response = false;
     echo json_encode($return);
     exit();
 }else if(isColumnFull($MOVE)){  //full column
     $return -> reason = "Column full, try another slot.";
+    $return -> response = false;
     echo json_encode($return);
     exit();
-}else if(!$MOVE){   //move is null
+}else if($MOVE == null){   //move is null
+    $return -> response = false;
     $return -> reason = "Please enter a move.";
     echo json_encode($return);
     exit();
 }
-
-$file = file_get_contents(url.$PID.".txt");
+$return -> slot = $MOVE;
+$file = file_get_contents($url.$PID.".txt");
 $decoded_file = json_decode($file);
 $strategy = &$decoded_file->strategy;
-$game_board = &$decoded_file->gameboard;
-$response = &$decoded_file->response;
-check();
+$game_board = &$decoded_file->board;
+$response = true;
+//echo json_encode(array($strategy));
+//echo json_encode(array($game_board));
+//echo json_encode(array($response));
+check($return);
 if($strategy == "Random"){
     $aimove = random_move();
 }else {
     $aimove = smart_move();
 }
-$ai_return = new move();
+$ai_return = new ackmove();
+$ai_return -> slot = $aimove;
+check($ai_return);
 
-update_board($aimove);
+
+
+
+$saved_board = $url.$PID.".txt";
+$opened_board = fopen($saved_board,"w");//creates new file
+$board_string = json_encode(array("pid"=>$PID, 'strategy'=> $strategy, 'board'=> $game_board));
+fwrite($opened_board,$board_string);
+fclose($opened_board);
+
+
 
 echo json_encode(array("response"=> $response,"ack_move" => array("slot"=> $MOVE,
     "isWin"=> $return->userWin, "isDraw"=> $return->userDraw,
-    "row"=> "[]"), "move"=> array("slot"=> $aimove, "isWin"=>$ai_return->isWin,
-    "isDraw"=> $ai_return->isDraw, "row"=> "[]")));
+    "row"=> array(), "move"=> array("slot"=> $aimove, "isWin"=>$ai_return->userWin,
+    "isDraw"=> $ai_return->userDraw, "row"=> array()))));
 
 function smart_move(){
     $smart_move = rand(0,6);
@@ -61,11 +79,10 @@ function random_move(){
 }
 function check($return)
 {
-    global $MOVE;
-    update_board($MOVE);
+    update_board($return->slot);
     $return->userWin = isWin();
     $return->userDraw = isDraw();
-    $return->slot = $MOVE;
+
 }
 function isWin(){
     global $game_board;
@@ -73,7 +90,7 @@ function isWin(){
 }
 function isDraw(){
     for($i = 0; $i <=6; $i++){
-        if(isColumnFull($i) && !isWin()){ //added !isWin
+        if(!isColumnFull($i)){ //added !isWin
             return false;
         }
     }
@@ -92,7 +109,7 @@ function update_board($input){
     for($i = 5; $i >=0;$i--){
         if ($game_board[$i][$input]== 0){
             $game_board[$i][$input]= 1;
-            exit();
+            $i = -1;
         }
     }
 }
@@ -103,15 +120,11 @@ class ackmove{ //human user
     var $reason;
     var $isWin;
     var $userDraw;
+    var $response;
 }
 //NOTE: no computer move if human has game ending move
 
-class move{ //computer logic
-    var $isDraw;
-    var $isWin;
-    var $slot;
-    var $row;
-}
+
 class gameInfo{
     public $WIDTH;
     public $HEIGHT;
